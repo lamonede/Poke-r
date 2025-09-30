16f6a772dafd55a61bacf54be0352d7abcca4540
lamonede <tlsdusgh1024@gmail.com>
2025-09-30 15:25:15 +0900

    Add new variable, funcrtion

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
+//Spade > Diamond > Clover > Heart
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define CARD_NUM 20
+
+int card_shape_checker(char a, char b);
 
 int main(){
-    int i;
-    i = 0;
+    int result = 0;
+    int num_of_people = 3;
+    char card[CARD_NUM] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    srand(time(NULL));
 
-    printf("%d", i);
+    for(int i = 0; i < 5+3*num_of_people; i++){
+        card[i] = rand()%61;
+    }
+    for(int i = 0; i < CARD_NUM-1; i++){
+        for(int j = 0; j < CARD_NUM; j++){
+            result = card_shape_checker(card[i], card[j]);
+            printf("%d\n", result);
+        }
+    }
+}
 
-    return 0;
+int card_shape_checker(char a, char b){
+    unsigned char result = ~(a^b); //xnor
+    return result;
 }
